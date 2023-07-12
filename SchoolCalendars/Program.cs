5cf443686f53269e4e3e08c60490aa1a352f2cf1
zahrablend <zahra@LAPTOP-010BK67M>
2023-07-12 21:55:05 +0300

    Add project files.

---
+++ SchoolCalendars/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolCalendars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
