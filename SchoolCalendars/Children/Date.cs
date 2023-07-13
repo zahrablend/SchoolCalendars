64201ced4b88eb06b3ef54deda4ebee3867275ce
zahrablend <zahra@LAPTOP-010BK67M>
2023-07-13 13:38:49 +0300

    Add private FirstValidDate method and implement AddYears method in Date class with its help. Implement FirstValidDate method in Date class using pattern matching (ternary expression)

--- SchoolCalendars/Children/Date.cs
+++ SchoolCalendars/Children/Date.cs
@@ -13,6 +13,14 @@ namespace SchoolCalendar.Children
             _day = day;
         }
 
+        public Date AddYears(int count) =>
+            FirstValidDate(_year + count, _day);
+
+        // Private as useful only for this class
+        // Pattern matching with ternary operator
+        private Date FirstValidDate(YearDate day) => day.IsLeap() && !IsLeap(year) ? new Date(year, day.GetNext())
+                : new Date(year, day);
+
         public Date GetFirstOccurence(YearDate day) => throw new NotImplementedException();
 
         public override string ToString() => _day + "/" + _year;
