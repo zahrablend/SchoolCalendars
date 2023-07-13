1f794b2b192599c9cd5539bbfe72c188ae165d6b
zahrablend <zahra@LAPTOP-010BK67M>
2023-07-13 12:41:10 +0300

    Add getDateByAge method to CHild class. Postpone method implementation

--- SchoolCalendars/Children/Child.cs
+++ SchoolCalendars/Children/Child.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolCalendar.SchoolLegalSystem;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,11 @@ namespace SchoolCalendar.Children
         {
             _name = name;
             _birthdate = birthdate;
+        }
+
+        public Date GetDateByAge(int age)
+        {
+            throw new NotImplementedException();
         }
 
         public override string ToString()
